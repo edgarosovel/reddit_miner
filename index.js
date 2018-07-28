6ad10036a2bc9121e242b145b6ba84ad069f0dff
edgarosovel <edgarov@live.com.mx>
2018-07-27 21:37:02 -0500

    Heroku Procfile

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var reddit = require('./libs/reddit')
 var log = require('./libs/log')
+const http = require('http')
 const config = require('./config')
 var subreddits = config.subreddits
 
@@ -9,3 +10,20 @@ console.log('Loading subreddits...');
 for (let subreddit of subreddits){
     reddit.watch_subreddit(subreddit);
 }
+
+//keep alive on heroku
+// setInterval(function () {
+//     var options = {
+//       host: 'google.com',
+//       port: 80,
+//       path: '/'
+//     };
+  
+//     http.get(options, function(res) {
+//       res.on('data', function(chunk) {
+//           //pass
+//       });
+//     }).on('error', function(err) {
+//         console.error(err);
+//     });
+//   }, 1800000); // Cada 30 minutos.
